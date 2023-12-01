afde90f927197a670c1f490bb84815590749aa13
Rigpea <rigpea@bu.edu>
2023-12-01 12:23:31 -0500

    everthing up to date

--- team/src/pages/LoggedInPage/LoggedInPage.js
+++ team/src/pages/LoggedInPage/LoggedInPage.js
@@ -1,40 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
 import UserInfo from "../../components/UserInfo.js";
 import './LoggedInPage.css';
 
 const LoggedInPage = () => {
-  const { user, logout } = useAuth0();
-  const [userData, setUserData] = useState(null);
-  const navigate = useNavigate();
+  const { logout } = useAuth0();
 
-  useEffect(() => {
-    const checkUser = async () => {
-      try {
-        const response = await axios.post('http://localhost:5000/api/check-user', { userId: user.sub });
-        if (response.data.exists) {
-          setUserData(response.data.userData);
-          // User exists, proceed with setting up initial image etc.
-        } else {
-          navigate('/registration'); // Redirect to registration page
-        }
-      } catch (error) {
-        console.error('Error checking user', error);
-      }
-    };
-
-    if (user) {
-      checkUser();
-    }
-  }, [user, navigate]);
-
-  if (!userData) {
-    return <div>Loading...</div>; // Or any other loading state
-  }
-
-  // Render user's data, initial image setup, etc.
   return (
     <div className="logged-in-container">
       <nav className="navigation-bar">
